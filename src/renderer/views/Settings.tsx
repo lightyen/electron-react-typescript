0cf1f29710f670c22f6abbc8bf8af649e754cbec
lightyen <lightyen0123@gmail.com>
2020-02-07 21:25:45 +0800

    update

--- src/renderer/views/Settings.tsx
+++ src/renderer/views/Settings.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
-import { useSelector, useAction } from "~/store"
 import { FormattedMessage } from "react-intl"
+import Select from "react-select"
+import { useSelector, useAction } from "~/store"
 
 const Page: React.FC = () => {
     const history = useHistory()
@@ -9,6 +10,7 @@ const Page: React.FC = () => {
     const textColor = useSelector(state => state.theme.textColor)
     const name = useSelector(state => state.i18n.name)
     const support = useSelector(state => state.i18n.support)
+    const options = Object.entries(support).map(([value, label]) => ({ value, label }))
     return (
         <div>
             <button
@@ -31,18 +33,13 @@ const Page: React.FC = () => {
                 <label className="block font-bold mb-2" style={{ color: textColor }}>
                     <FormattedMessage id="language" />
                 </label>
-                <div>
-                    <select
-                        value={name}
-                        onChange={e => setLocale(e.target.value)}
-                        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    >
-                        {Object.keys(support).map(v => (
-                            <option key={v} value={v}>
-                                {support[v]}
-                            </option>
-                        ))}
-                    </select>
+                <div className="w-64">
+                    <Select
+                        className="text-blue-500"
+                        options={options}
+                        value={options.find(v => v.value == name)}
+                        onChange={e => setLocale(e["value"])}
+                    />
                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
