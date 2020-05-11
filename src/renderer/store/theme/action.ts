ec8258ad0328e90cb0d8375b9c6fd072b29b21ee
lightyen <lightyen0123@gmail.com>
2020-05-11 23:07:47 +0800

    fix

--- src/renderer/store/theme/action.ts
+++ src/renderer/store/theme/action.ts
@@ -2,7 +2,7 @@ import { ThemeName } from "./themes"
 import { createAction } from "@reduxjs/toolkit"
 
 export const changeTheme = createAction("CHANGE_THEME", (payload: { name: ThemeName }) => {
-	localStorage.setItem("theme", name)
+	localStorage.setItem("theme", payload.name)
 	return { payload }
 })
 
