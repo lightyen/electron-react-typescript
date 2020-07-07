23a2690c5c409e9c9455b78bf16d086a2f41ba9e
lightyen <lightyen0123@gmail.com>
2020-07-07 15:46:27 +0800

    update

--- src/renderer/components/FileUploader.tsx
+++ src/renderer/components/FileUploader.tsx
@@ -2,26 +2,29 @@ import React from "react"
 import { dndimages } from "@shared/ipc"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFileUpload } from "@fortawesome/free-solid-svg-icons/faFileUpload"
+import classnames from "classnames"
 
 const FileUploader: React.FC = () => {
+	const [hover, setHover] = React.useState(false)
 	return (
-		<div
-			className="flex flex-col"
-			style={{ width: 300, height: 220 }}
-			onDragOver={e => e.preventDefault()}
-			onDrop={async e => {
-				e.preventDefault()
-				dndimages.send(Array.from(e.dataTransfer.files).map(v => v.path))
-				console.log(Array.from(e.dataTransfer.files).map(v => v.type))
-			}}
-		>
+		<div className="flex flex-col cursor-pointer" style={{ width: 300, height: 220 }}>
 			<div
-				className="flex flex-grow justify-center items-center m-3 border-file-uploader"
+				className={classnames("flex flex-grow justify-center items-center m-3 border-file-uploader", { hover })}
 				onClick={async () => {
 					dndimages.send()
 				}}
+				onDragOver={e => {
+					e.preventDefault()
+				}}
+				onDragEnter={e => setHover(true)}
+				onDragLeave={e => setHover(false)}
+				onDrop={async e => {
+					e.preventDefault()
+					dndimages.send(Array.from(e.dataTransfer.files).map(v => v.path))
+					console.log(Array.from(e.dataTransfer.files).map(v => v.type))
+				}}
 			>
-				<FontAwesomeIcon icon={faFileUpload} size="2x" />
+				<FontAwesomeIcon className="pointer-events-none" icon={faFileUpload} size="2x" />
 			</div>
 		</div>
 	)
